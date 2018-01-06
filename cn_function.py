0ebdfdcf8fe0fd4b99fea7fe88eeb3b60a869c57
mordy <mordydd91@gmail.com>
2018-01-06 12:54:23 +0200

    add try and exute

--- cn_function.py
+++ cn_function.py
@@ -1,18 +1,21 @@
 import json
 import cloudinary
-import cloudinary
+
 cloudinary.config(
   cloud_name = 'akaton6',
   api_key = '932415597598148',
   api_secret = '_mEs3TgwRoZoJblmRCbJq03ZkXU'
 )
 
-
 def open_file():
-    with open('DATA.json')as f:
-        data = json.load(f)
-    f.close()
-    return data
+    try:
+        with open('DATA.json') as f:
+            data = json.load(f)
+    except Exception as e:
+        print(e)
+    else:
+        f.close()
+        return data
 
 def delete_photo(id):
     cloudinary.uploader.destroy(id)
@@ -27,16 +30,20 @@ def login(username,password):
     return False
 
 def register(json_cv):
-    with open(json_cv)as f:
-        cv = json.load(f)
-    f.close()
-    data = open_file()
-    data.append(cv)
-    f = open('DATA.json', 'w')
-    json.dump(data, f)
-    f.close()
-    # upload_photo(cv)
-    return True
+    try:
+        with open(json_cv)as f:
+            cv = json.load(f)
+    except Exception as e:
+        print(e)
+    else:
+        f.close()
+        data = open_file()
+        data.append(cv)
+        f = open('DATA.json', 'w')
+        json.dump(data, f)
+        f.close()
+        # upload_photo(cv)
+        return True
 
 def delete_cv(id):
     data = open_file()
@@ -48,14 +55,18 @@ def delete_cv(id):
                 data.remove(i)
                 delete_photo(id)
             break
-    f=open('DATA.json','w')
-    json.dump(data,f)
-    f.close()
-    if flag:
-        return True
+    try:
+        f = open('DATA.json', 'w')
+    except Exception as e:
+        print(e)
     else:
-        print("this id doesnt exist or status!=2")
-        return False
+        json.dump(data, f)
+        f.close()
+        if flag:
+            return True
+        else:
+            print("this id doesnt exist or status!=2")
+            return False
 
 def change_mobile(id, new_mobile):
     flag=0
@@ -65,12 +76,16 @@ def change_mobile(id, new_mobile):
             flag=1
             i['personal_summary']['mobile']=new_mobile
             break
-    f = open('DATA.json', 'w')
-    json.dump(data, f)
-    f.close()
-    if flag:
-        return True
-    return False
+    try:
+        f = open('DATA.json', 'w')
+    except Exception as e:
+        print(e)
+    else:
+        json.dump(data, f)
+        f.close()
+        if flag:
+            return True
+        return False
 
 def change_mail(id, new_mail):
     flag=0
@@ -80,9 +95,13 @@ def change_mail(id, new_mail):
             flag=1
             i['personal_summary']['email']=new_mail
             break
-    f = open('DATA.json', 'w')
-    json.dump(data, f)
-    f.close()
-    if flag:
-        return True
-    return False
+    try:
+        f = open('DATA.json', 'w')
+    except Exception as e:
+        print(e)
+    else:
+        json.dump(data, f)
+        f.close()
+        if flag:
+            return True
+        return False
